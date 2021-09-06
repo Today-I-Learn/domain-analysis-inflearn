b2e90b5812ac0537f9c368d4386cca65b686db93
ssibongee <ssibongee@gmail.com>
2021-09-06 16:23:30 +0900

    init: 인프런 도메인 분석을 위한 프로젝트 생성

---
+++ src/main/java/com/inflearn/InflearnApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inflearn/InflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
